ef0449b55da8cd4f56c8b46455f411eaa600c430
Karn Gulati <karngulati@gmail.com>
2019-03-05 23:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,6 +73,5 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else ()
-  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
-  setCart(emptyCart)
+  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
 }
